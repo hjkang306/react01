f014b720fc333a8a227e1232c9496bb17a83824f
hjkang306 <hjkang306@gmail.com>
2022-11-22 09:57:22 +0900

    2022.11.22

--- react_youtube/src/components/ChennelConts.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const ChennelConts = () => {
-  return <div>ChennelConts</div>
-}
-
-export default ChennelConts
--- react_youtube/src/components/SearchConts.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const SearchConts = () => {
-  return <div>SearchConts</div>
-}
-
-export default SearchConts
